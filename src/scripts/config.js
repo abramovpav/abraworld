e3e980356d8618b9f8e0e2e5a4d418104b3a91ef
Pavel Abramov <abramovpav@gmail.com>
2015-07-18 03:12:09 +0300

    Enable states

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -3,7 +3,7 @@
 module.exports = /*@ngInject*/
     function ($stateProvider, $urlRouterProvider, $locationProvider) {
         // For any unmatched url, redirect to /state1
-        $urlRouterProvider.otherwise("/");
+        $urlRouterProvider.otherwise("/articles");
         $locationProvider.html5Mode(true);
 
         // Now set up the states
@@ -16,19 +16,19 @@ module.exports = /*@ngInject*/
                 abstract: true,
                 templateUrl: "base"
             })
-            .state('app.common.nothing', {
-                url: '/',
-                templateUrl: "nothing/index"
-            })
-            //.state('app.common.profile', {
-            //    url: "/profile",
-            //    templateUrl: "profile/index"
+            //.state('app.common.nothing', {
+            //    url: '/',
+            //    templateUrl: "nothing/index"
             //})
-            //.state('app.common.state2.list', {
-            //    url: "/list",
-            //    templateUrl: "state2/state2.list",
-            //    controller: /*@ngInject*/function ($scope) {
-            //        $scope.things = ["A", "Set", "Of", "Things"];
-            //    }
-            //});
+            .state('app.common.profile', {
+                url: "/profile",
+                templateUrl: "profile/index"
+            })
+            .state('app.common.state2.list', {
+                url: "/list",
+                templateUrl: "state2/state2.list",
+                controller: /*@ngInject*/function ($scope) {
+                    $scope.things = ["A", "Set", "Of", "Things"];
+                }
+            });
     };
