8e51e481d128261a43cb7b6c684858b219a6bcf4
Pavel Abramov <abramovpav@gmail.com>
2015-12-21 11:49:32 +0300

    Updated login page

--- src/scripts/modules/auth/controllers/LoginController.js
+++ src/scripts/modules/auth/controllers/LoginController.js
@@ -1,29 +1,34 @@
 'use strict';
 
 module.exports = /*@ngInject*/
-    function LoginController($scope, AuthDataService, $state, $stateParams, $cookies, UsersDataService) {
+    function LoginController($scope, AuthDataService, $state, $stateParams, $cookies, UsersDataService, $rootScope) {
 
         window.log = $scope;
         window.cook = $cookies;
 
         $scope.login = function () {
-            //var _csrf = $cookies.get('csrftoken');
             var _authData = angular.copy($scope.credentials);
-            //_authData.csrfmiddlewaretoken = _csrf;
-            //$http.defaults.headers.common['qweasd'] = 'asdas';
+
             return AuthDataService.auth.login(_authData).then(
                 function (response) {
                     console.log(response);
                     if (response.token)
                         $cookies.put('jwt', response.token);
                     return response
+                },
+                function (response) {
+                    if (response.non_field_errors)
+                        $scope.error = response.non_field_errors;
+                    else
+                        $scope.error = 'Unable to login';
                 }
             ).then(
                 function() {
                     return UsersDataService.user.current().then(
                         function(response) {
                             console.log(response);
-                            $scope.user = response
+                            $rootScope.user = response;
+                            $state.go('app.common.blog.list');
                         },
                         function (response) {
                             $scope.error = response
@@ -32,23 +37,6 @@ module.exports = /*@ngInject*/
                 }
             );
         };
-
-        //function getCookie(name) {
-        //    var cookieValue = null;
-        //    if (document.cookie && document.cookie != '') {
-        //        var cookies = document.cookie.split(';');
-        //        for (var i = 0; i < cookies.length; i++) {
-        //            var cookie = jQuery.trim(cookies[i]);
-        //            // Does this cookie string begin with the name we want?
-        //            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-        //                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        //                break;
-        //            }
-        //        }
-        //    }
-        //    return cookieValue;
-        //}
-
 
         function init() {
             $scope.creadentials = {};
